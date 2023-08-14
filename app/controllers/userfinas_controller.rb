92de99fead68a669f28bac4a6638389f4a2e3601
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-14 14:20:45 +0300

    create method

--- app/controllers/userfinas_controller.rb
+++ app/controllers/userfinas_controller.rb
@@ -1,21 +1,30 @@
 class UserfinasController < ApplicationController
     def index
-        userfinas = Userfina.all
+      userfinas = Userfina.all
+      render json: userfinas
+    end
+  
+    def show
+      userfina = Userfina.find_by(users_params)
+      if !userfina
+        head :not_found
+      else
         render json: {
-            userfinas
-        }
+          userfina: userfina,
+          users: userfina.users
+        }, status: :ok
+      end
     end
 
-    def show
-        userfina = Userfina.find_by(users_params)
-        if !userfina
-            return head :not_found
-            else
-                render json: {
-                    userfina,
-                    users: userfina.users
-                    }, status: 201
-            end
+    def create
+        userfina = Userfina.create(userfina_params)
+        if userfina.valid?
+          render json: userfina, status: :created
+        else
+          render json: {
+            errors: userfina.errors.full_messages
+          }, status: :unprocessable_entity
         end
-    end
-end
+      end
+ end
+  
