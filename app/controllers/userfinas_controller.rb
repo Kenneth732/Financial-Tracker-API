a793bf93dbdfbbe38b1cf2c8bec038a1bde1a3a8
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-14 14:23:03 +0300

    create

--- app/controllers/userfinas_controller.rb
+++ app/controllers/userfinas_controller.rb
@@ -6,25 +6,38 @@ class UserfinasController < ApplicationController
   
     def show
       userfina = Userfina.find_by(users_params)
-      if !userfina
-        head :not_found
-      else
+      if userfina
         render json: {
           userfina: userfina,
           users: userfina.users
         }, status: :ok
+      else
+        head :not_found
       end
     end
-
+  
     def create
-        userfina = Userfina.create(userfina_params)
-        if userfina.valid?
-          render json: userfina, status: :created
-        else
-          render json: {
-            errors: userfina.errors.full_messages
-          }, status: :unprocessable_entity
-        end
+      userfina = Userfina.create(userfina_params)
+      if userfina.valid?
+        render json: userfina, status: :created
+      else
+        render json: {
+          errors: userfina.errors.full_messages
+        }, status: :unprocessable_entity
       end
- end
+    end
+  
+    def update
+      userfina = Userfina.find(params[:id])
+      if userfina.update(userfina_params)
+        render json: userfina, status: :ok
+      else
+        render json: {
+          errors: userfina.errors.full_messages
+        }, status: :unprocessable_entity
+      end
+    end
+  
+
+  end
   
