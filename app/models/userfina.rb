af19ba40e353185486f68e4f3942fc626585c0cf
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-14 14:26:40 +0300

    validation

--- app/models/userfina.rb
+++ app/models/userfina.rb
@@ -1,2 +1,5 @@
 class Userfina < ApplicationRecord
+    validates :name, presence: true
+    validates :income, presence: true, numericality: { greater_than: 0 }
+    validates :year, presence: true, numericality: { only_integer: true, greater_than: 0 }
 end
